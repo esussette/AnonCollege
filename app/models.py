ab340b96a23031d0a003b7e3bba5b545742b7fe9
esussette <eobando@ithaca.edu>
2020-12-01 22:37:20 -0500

    models & init wip

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 
 
@@ -7,7 +10,25 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     pass_hash = db.relationship('Post', backref='author', lazy='dynamic')
 
+ def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
 
 
