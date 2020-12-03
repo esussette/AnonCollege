a02d4b5ed3bc6cfac945946d8e1b312e6851860b
esussette <eobando@ithaca.edu>
2020-12-03 02:37:40 -0500

    modified: base,routes,models created: config,topic,forms

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    pass_hash = db.relationship('Post', backref='author', lazy='dynamic')
+    password = db.relationship('Post', backref='author', lazy='dynamic')
 
  def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,7 +18,7 @@ class User(db.Model):
 
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
 from random import random
-
 from flask import render_template
-
 from app import app
 
-
+@app.route('/')
 @app.route("/quote")
 def random_quote():
     quotes = [
@@ -13,3 +11,22 @@ def random_quote():
         "You miss 100% of the shots you don't take."]
     idx = random.randrange(len(quotes))
     return render_template('login.html', q=quotes[idx])
+
+@app.route('/')
+@app.route('/topic')
+#Example of routing to a page of posts - this route name is a placeholder bc we
+#have not decided on those specifics yet
+def the_posts():
+    user = {'username': 'User'}
+    posts = {
+        {
+            'author': {'username': 'poster'}
+            'body' : 'This is a post'
+        },
+        {
+            'author': {'username': 'poster'},
+            'body': 'This is another post'
+        }
+    }
+    return render_template('topic.html', title='Topic Page', user=user, posts=posts)
+
