e6f8544518e77cf4ec8680301928e9d81311174b
Lillyann Mena <lillyannmena.lm@gmail.com>
2020-12-07 21:54:22 -0500

    formatting adjustments

--- app/models.py
+++ app/models.py
@@ -10,15 +10,15 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.relationship('Post', backref='author', lazy='dynamic')
 
- def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+ # def __repr__(self):
+ #        return '<User {}>'.format(self.username)
+ #
+ #    def set_password(self, password):
+ #        self.password_hash = generate_password_hash(password)
+ #
+ #
+ #    def check_password(self, password):
+ #        return check_password_hash(self.password, password)
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from random import random
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route("/quote")
 def random_quote():
@@ -12,21 +13,15 @@ def random_quote():
     idx = random.randrange(len(quotes))
     return render_template('login.html', q=quotes[idx])
 
+
 @app.route('/')
 @app.route('/topic')
-#Example of routing to a page of posts - this route name is a placeholder bc we
-#have not decided on those specifics yet
+# Example of routing to a page of posts - this route name is a placeholder bc we
+# have not decided on those specifics yet
 def the_posts():
     user = {'username': 'User'}
     posts = {
-        {
-            'author': {'username': 'poster'}
-            'body' : 'This is a post'
-        },
-        {
-            'author': {'username': 'poster'},
-            'body': 'This is another post'
-        }
+        {'author': {'username': 'poster'}, 'body': {'This is a post'}},
+        {'author': {'username': 'poster'}, 'body': {'This is another post'}}
     }
     return render_template('topic.html', title='Topic Page', user=user, posts=posts)
-
