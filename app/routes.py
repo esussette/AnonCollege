f816b29cfefc10072975d35a4647be5155fc1ea9
esussette <eobando@ithaca.edu>
2020-12-09 23:56:28 -0500

    bug fixes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user
+from flask_login import current_user, login_user, logout_user
 from sqlalchemy.sql.functions import random
 from werkzeug.urls import url_parse
 
@@ -9,8 +9,8 @@ from app.models import User
 
 
 @app.route('/')
-@app.route('/home', methods=['GET', 'POST'])
-def homepage():
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 
@@ -37,15 +37,16 @@ def login():
     #     return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(username=form.username).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('home'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('home')
         return redirect(next_page)
+        return redirect((url_for('home')))
     return render_template('login.html', title='Sign In', form=form)
 
 
@@ -58,7 +59,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     # if current_user.is_authenticated:
-    #     return redirect(url_for('index'))
+    #     return redirect(url_for('login'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -66,7 +67,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
 
 
