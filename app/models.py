a6ec227e8eb2745c99b499b7d87b00e5214aa78c
esussette <eobando@ithaca.edu>
2020-12-15 15:40:05 -0500

    following/followers

--- app/models.py
+++ app/models.py
@@ -9,6 +9,12 @@ from werkzeug.utils import redirect
 
 from app import db, login, app
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,14 +24,38 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -53,7 +83,6 @@ class Post(db.Model):
         return '<Post {}>'.format(self.body)
 
 
-
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
@@ -62,19 +91,13 @@ class Feedback(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-            return '<Feedbacl {}>'.format(self.body)
+        return '<Feedbacl {}>'.format(self.body)
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
     categories = Post.category.desc()
-
-
-
-
-
-
 
 # # *//*
 # class CategoryToPost(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -119,14 +119,14 @@ def user(username):
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
-def edit():
+def edit_profile():
     form = EditProfileForm()
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit'))
+        return redirect(url_for('edit_profile'))
     else:
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
